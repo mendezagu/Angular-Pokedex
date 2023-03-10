a93f4840912ffb5a6e99527435f55313de137075
zagmen <mendezagustin.ma2@gmail.com>
2023-03-10 13:56:30 -0300

    modificacion en el metodo getPokemon

--- src/app/components/poke-details/poke-details.component.ts
+++ src/app/components/poke-details/poke-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-poke-details',
@@ -24,7 +25,14 @@ export class PokeDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.pokemonSubject.subscribe(
+      res => {
+        console.log(res); // Aquí manejas la respuesta emitida por el observable
+      }
+    );
   }
+
+  pokemonSubject = new Subject<any>();
 
   getPokemon(id: any) {
     this.pokemonService.getPokemons(id).subscribe(
@@ -33,6 +41,8 @@ export class PokeDetailsComponent implements OnInit {
         this.pokemon = res;
         this.pokemonImg = this.pokemon.sprites.front_default;
         this.pokemonType = res.types[0].type.name;
+
+        this.pokemonSubject.next(res); // Emite la respuesta del servicio utilizando el método next()
       },
       err => {
         console.log(err);
